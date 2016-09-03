5fee908cb6045ebf726c5534ca7d452220008d7e
XadillaX <admin@xcoder.in>
2016-09-03 16:09:08 +0800

    feat: international sending finished

--- example/example.js
+++ example/example.js
@@ -8,7 +8,17 @@
 
 const IHuyi = require("../");
 
-const iHuyi = new IHuyi(process.env.IHUYI_ACCOUNT, process.env.IHUYI_PASSWORD);
+const iHuyi = new IHuyi(
+    process.env.IHUYI_ACCOUNT,
+    process.env.IHUYI_PASSWORD,
+    process.env.IHUYI_INTERNATIONAL_APIKEY);
+
+iHuyi.sendInternational(process.env.IHUYI_CONTRY_CODE,
+    process.env.IHUYI_INTERNATIONAL_MOBILE,
+    "[fei.me]Your verification code is 0000, do not disclosed to others.",
+    function(err, smsId) {
+        console.log(err, smsId);
+    });
 
 iHuyi.send(process.env.MY_MOBILE,
     "您的验证码是：0000。请不要把验证码泄露给其他人。",
